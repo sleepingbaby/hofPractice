b4baf24ff3776db0cc8495f2134679a82b6d8ebc
Kelvin <crosby2987@gmail.com>
2021-11-12 12:59:06 -0800

    Completed all implementations

--- practice.js
+++ practice.js
@@ -62,19 +62,33 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-
+  return _.reduce(products, function(total, number) {
+    number = Number(number['price'].slice(number['price'].indexOf('$')+1, number['price'].length));
+    return total + number;
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
-
+  return _.reduce(desserts, function(acc, dessert) {
+    if (acc[dessert['type']] === undefined) {
+      acc[dessert['type']] = 1;
+    } else {
+      acc[dessert['type']]++;
+    }
+    return acc;
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
+  return _.reduce(movies, function(acc, movie) {
+    if (movie.releaseYear > 1990 && movie.releaseYear < 2000) { acc.push(movie.title); }
+    return acc;
+  }, []);
 
 };
 
@@ -82,7 +96,10 @@ var ninetiesKid = function(movies) {
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function(movies, timeLimit) {
-
+  return _.reduce(movies, function(acc, movie) {
+    if (movie.runtime < timeLimit) { acc = true; }
+    return acc;
+  }, false);
 };
 
 /*
@@ -94,14 +111,22 @@ var movieNight = function(movies, timeLimit) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function(fruits) {
-
+  return _.map(fruits, function(fruit) { return fruit.toUpperCase(); });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function(desserts) {
-
+  return _.map(desserts, function(dessert) {
+    console.log(dessert['ingredients'].includes('flour'));
+    if (dessert['ingredients'].includes('flour')) {
+      dessert['glutenFree'] = false;
+    } else {
+      dessert['glutenFree'] = true;
+    }
+    return dessert;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -125,5 +150,10 @@ var glutenFree = function(desserts) {
 
 */
 var applyCoupon = function(groceries, coupon) {
-
+  return _.map(groceries, function(grocery) {
+    var price = Number(grocery['price'].slice(grocery['price'].indexOf('$')+1, grocery['price'].length));
+    var salePrice = price - ((Math.round(price * coupon * 100) / 100));
+    grocery['salePrice'] = '$' + salePrice;
+    return grocery;
+  });
 };
